2237b03f3808875cb03ffbf01b2ac9d216de20ae
Tony Varlamov <tonywarlamoff@gmail.com>
2018-10-01 12:48:56 +0300

    add responsive font size

--- src/views/app/index.jsx
+++ src/views/app/index.jsx
@@ -8,6 +8,12 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <h1>Hello UI!</h1>
+        <h2>Hello</h2>
+        <h3>Hello</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic voluptatem dolorem optio laboriosam minus ea commodi mollitia, ab reiciendis explicabo enim illo nulla? Ut iusto dicta voluptatibus reprehenderit facilis!</p>
+        <a href="https://github.com/AllThingsSmitty/css-protips"></a>
+        <p></p>
+        <a href="https://github.com/AllThingsSmitty/css-protips">CSS Protips</a>
       </React.Fragment>
     );
   }
