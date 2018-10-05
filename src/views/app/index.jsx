febecc98504ae1c4aad7072d9f129cd2d209f600
Tony Varlamov <tonywarlamoff@gmail.com>
2018-10-05 14:58:15 +0300

    test font size

--- src/views/app/index.jsx
+++ src/views/app/index.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import './reset';
 import './style';
 
+import Wrapper from '../../components/wrapper';
+import Container from '../../components/container';
+
 class App extends React.Component {
   render() {
     return (
@@ -10,10 +13,49 @@ class App extends React.Component {
         <h1>Hello UI!</h1>
         <h2>Hello</h2>
         <h3>Hello</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic voluptatem dolorem optio laboriosam minus ea commodi mollitia, ab reiciendis explicabo enim illo nulla? Ut iusto dicta voluptatibus reprehenderit facilis!</p>
-        <a href="https://github.com/AllThingsSmitty/css-protips"></a>
-        <p></p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic
+          voluptatem dolorem optio laboriosam minus ea commodi mollitia, ab
+          reiciendis explicabo enim illo nulla? Ut iusto dicta voluptatibus
+          reprehenderit facilis!
+        </p>
+        <a href="https://github.com/AllThingsSmitty/css-protips" />
+        <p />
         <a href="https://github.com/AllThingsSmitty/css-protips">CSS Protips</a>
+        <Wrapper>
+          <Container>
+            <h1>Hello UI!</h1>
+            <h2>Hello</h2>
+            <h3>Hello</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic
+              voluptatem dolorem optio laboriosam minus ea commodi mollitia, ab
+              reiciendis explicabo enim illo nulla? Ut iusto dicta voluptatibus
+              reprehenderit facilis!
+            </p>
+            <a href="https://github.com/AllThingsSmitty/css-protips" />
+            <p />
+            <a href="https://github.com/AllThingsSmitty/css-protips">
+              CSS Protips
+            </a>
+          </Container>
+        </Wrapper>
+        <Wrapper fullscreen>
+          <h1>Hello UI!</h1>
+          <h2>Hello</h2>
+          <h3>Hello</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum hic
+            voluptatem dolorem optio laboriosam minus ea commodi mollitia, ab
+            reiciendis explicabo enim illo nulla? Ut iusto dicta voluptatibus
+            reprehenderit facilis!
+          </p>
+          <a href="https://github.com/AllThingsSmitty/css-protips" />
+          <p />
+          <a href="https://github.com/AllThingsSmitty/css-protips">
+            CSS Protips
+          </a>
+        </Wrapper>
       </React.Fragment>
     );
   }
