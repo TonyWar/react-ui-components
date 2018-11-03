057b436b9830ecb7439254a8c48002a416bc5a8b
Tony Varlamov <tonywarlamoff@gmail.com>
2018-11-03 23:24:30 +0300

    add cubes loader

---
+++ src/components/UI/loaders/cubes/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './style';
+
+export default class Loader extends React.Component {
+  render() {
+    return (
+      <div class="cube-loader">
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    );
+  }
+}
