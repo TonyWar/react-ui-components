2a8533b0e31b7ca1cbc208b5760b6b6ceee90c7a
Tony Varlamov <tonywarlamoff@gmail.com>
2018-11-03 23:24:25 +0300

    add simple loader

---
+++ src/components/UI/loaders/flat-loader/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style'
+
+export default class Loader extends React.Component {
+  render() {
+    return (
+      <div class="flat-loader light"></div>
+    )
+  }
+}
